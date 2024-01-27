bc605878dad41132f7956c744645c1ef0c8c54cd
Ghislain Di Paolo <ghis.dp@outlook.com>
2024-01-27 22:50:29 +0100

    Small update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # MistralCall
+
+This is a program that lets you talk to an artificial intelligence (like a helper robot) named Mistral (https://mistral.ai). You can type a question in the "Question" box, select the size of Mistral's brain (small, medium, or tiny) using the dropdown, and then press the "Send" button to get an answer. The answers and questions will appear in the "History" box so you can look back at them. You can also delete, copy, or clear the questions and answers.
+
+The program uses something called 'Mistral Client' and 'PySimpleGUI' to create the window and talk to the Mistral AI. It also uses 'threading' to make sure the program doesn't freeze while waiting for an answer from Mistral. The 'pyperclip' library allows you to copy and paste answers easily. The program also keeps track of all the questions and answers in a list called 'history_list'.
+
+### Notes
+- The History is actually sent to the AI to have a conversation and context so it costs tokens, but you can delete some or all lines.
+- Very beta state, feel free to commit anything.
+
+## Installation
+```
+pip install -r requirements
+echo 'mistral_api_key' >> .api_key
+```
+
+## Run
+```
+sh mistral.sh 
+```
+
+![img.png](img.png)
--- main.py
+++ main.py
@@ -34,8 +34,13 @@ layout = [
 def _get_history():
     messages = []
 
+    if len(history_list) > 0 and history_list[0].startswith('#assistant'):
+        raise Exception("History must start with a user question")
+
     for history_question in history_list:
-        if history_question.startswith('#user'):
+        if history_question.startswith('#error'):
+            continue
+        elif history_question.startswith('#user'):
             history_question = history_question[6:]
             messages.append(ChatMessage(role="user", content=history_question))
         elif history_question.startswith('#assistant'):
@@ -50,23 +55,26 @@ def _get_history():
 def ask_ai(question, model):
     global is_receiving
 
-    messages = _get_history()
-    messages.append(ChatMessage(role="user", content=question))
+    try:
+        messages = _get_history()
+        messages.append(ChatMessage(role="user", content=question))
 
-    print("Question: " + question)
-    is_receiving = True
-    full_response = ''
-    for chunk in client.chat_stream(model=model, messages=messages):
-        response = chunk.choices[0].delta.content
-        print(response, end='')
-        full_response += response
-        queue.put(response)
+        print("Question: " + question)
+        is_receiving = True
+        full_response = ''
+        for chunk in client.chat_stream(model=model, messages=messages):
+            response = chunk.choices[0].delta.content
+            print(response, end='')
+            full_response += response
+            queue.put(response)
 
-    history_list.append("#user " + question)
-    history_list.append("#assistant " + full_response)
-
-    time.sleep(0.5)
-    is_receiving = False
+        history_list.append("#user " + question)
+        history_list.append("#assistant " + full_response)
+    except Exception as e:
+        history_list.append(f"#error {e}")
+    finally:
+        time.sleep(0.5)
+        is_receiving = False
 
 
 def update_response():
@@ -93,58 +101,54 @@ window = sg.Window('Mistral Call', layout, resizable=True)
 
 # Event Loop
 while True:
-    event, values = window.read(timeout=0)
-    if event == sg.WIN_CLOSED:
-        break
-    elif event == 'Send':
-        window['Answer'].update('')
-        model_size = values["cb_model"]
-        window['mistral_answer_label'].update(f"Answer [{model_size}]")
-        question = values['user_question']
-        thread = threading.Thread(target=ask_ai, args=(values['user_question'], model_size))
-        thread.start()
-        is_receiving = True
-    elif event == 'Clear':
-        window['user_question'].update('')
-        window['Answer'].update('')
-    elif event == 'Delete All':
-        history_list = []
-        window['HistoryList'].update([])
-    elif event == 'Delete':
-        if values['HistoryList']:
-            selected_text = values['HistoryList'][0]
-            for i in range(len(history_list)):
-                if history_list[i][:200] == selected_text[:200]:
-                    history_list.pop(i)
-                    break
-        window['HistoryList'].update(history_list)
-    elif event == 'Copy':
-        if values['HistoryList']:
-            selected_text = values['HistoryList'][0]    # Get the first selected item
-            pyperclip.copy(selected_text)               # Copy to clipboard
-    elif event == 'Show':
-        if values['HistoryList']:
-            selected_text = values['HistoryList'][0]
-            show_popup_selected(selected_text)
-        else:
-            sg.popup('No item selected')
-    elif event == 'HistoryList<DoubleClick>':
-        selected_text = values['HistoryList'][0]
-        show_popup_selected(selected_text)
+    try:
+        event, values = window.read(timeout=100)
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == 'Send':
+            window['Send'].update(disabled=True)
+            window['Answer'].update('')
+            is_receiving = True
 
-    if is_loop_receive == 1:
-        while is_receiving:
-            update_response()
-            time.sleep(.1)
-        if not is_receiving:
-            is_loop_receive = 2
-    elif is_loop_receive == 2:
-        window['HistoryList'].update(history_list)
-        is_loop_receive = 0
-        window['Send'].update(disabled=False)
-    elif is_receiving:
-        is_loop_receive = 1
-        window['Send'].update(disabled=True)
-    time.sleep(.1)
+            model_size = values["cb_model"]
+            window['mistral_answer_label'].update(f"Mistral [{model_size}] @ {time.strftime('%Y-%m-%d %H:%M:%S')}:")
+
+            thread = threading.Thread(target=ask_ai, args=(values['user_question'], model_size))
+            thread.start()
+        elif event == 'Clear':
+            window['user_question'].update('')
+            window['Answer'].update('')
+        elif event == 'Delete All':
+            history_list = []
+            window['HistoryList'].update([])
+        elif event == 'Delete':
+            if values['HistoryList']:
+                selected_text = values['HistoryList'][0]
+                for i in range(len(history_list)):
+                    if history_list[i][:200] == selected_text[:200]:
+                        history_list.pop(i)
+                        break
+            window['HistoryList'].update(history_list)
+        elif event == 'Copy':
+            if values['HistoryList']:
+                selected_text = values['HistoryList'][0]    # Get the first selected item
+                pyperclip.copy(selected_text)               # Copy to clipboard
+        elif event == 'Show':
+            if values['HistoryList']:
+                selected_text = values['HistoryList'][0]
+                show_popup_selected(selected_text)
+            else:
+                sg.popup('No item selected')
+
+        if is_receiving:
+            while is_receiving:
+                update_response()
+                time.sleep(.05)
+            window['HistoryList'].update(history_list)
+            is_loop_receive = 0
+            window['Send'].update(disabled=False)
+    except Exception as e:
+        window['mistral_answer_label'].update(f"ERROR : {e}")
+        time.sleep(1)
 
 window.close()
